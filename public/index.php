72dd59ca84f96a47900f96a23510ef140beb68cc
Tiago Sabbioni <46331236+tiagosabbioni@users.noreply.github.com>
2026-06-10 15:01:02 -0400

    fix: Security headers added to fix Lighthouse report

--- public/index.php
+++ public/index.php
@@ -11,13 +11,17 @@ $app = AppFactory::create();
 
 $app->addBodyParsingMiddleware();
 
-// CORS Middleware to allow requests from the frontend
+// CORS and security headers middleware
 $app->add(function ($request, $handler) {
 	$response = $handler->handle($request);
 	return $response
 		->withHeader('Access-Control-Allow-Origin', '*')
 		->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+		->withHeader('X-Frame-Options', 'DENY')
+		->withHeader('X-Content-Type-Options', 'nosniff')
+		->withHeader('Cross-Origin-Opener-Policy', 'same-origin')
+		->withHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
 });
 
 $app->options('/{routes:.+}', function ($request, $response) {
